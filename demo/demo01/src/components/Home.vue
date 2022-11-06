f83fc0c0fe1727c2df7f6ca7e3453a4e1e3da790
1qiqiqi <1377231170@qq.com>
2022-11-06 09:14:22 +0800

    v1.0

---
+++ demo/demo01/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Home!!!!!!!!!!!!!</h1>
+        <h1>{{ msg }}</h1>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    props: {
+        msg: String
+    }
+}
+</script>
